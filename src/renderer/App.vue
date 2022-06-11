4ba21e1d9a92c0b3722611b532f2eca40187ca2c
ObnoxiousOliver <maxhd2005@gmail.com>
2022-06-11 17:22:24 +0200

    dev-feat(home-view): temporary logout button

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -3,40 +3,37 @@
 </template>
 
 <script lang="ts" setup>
-import { getAuth, onAuthStateChanged } from '@firebase/auth'
-import { onBeforeUnmount, onMounted } from '@vue/runtime-core'
-import { reloadUser, updateUserState } from './firebase/auth'
 import AppLayout from './layout/appLayout/AppLayout.vue'
 
 // Handle Auth
-const auth = getAuth()
+// const auth = getAuth()
 
-onMounted(() => {
-  let reloadIntervalId: number | undefined
+// onMounted(() => {
+//   let reloadIntervalId: number | undefined
 
-  // Listen for auth state changes
-  const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
-    updateUserState(user)
-    if (user) {
-      if (reloadIntervalId) {
-        clearInterval(reloadIntervalId)
-      }
+//   // Listen for auth state changes
+//   const unsubscribeAuth = onAuthStateChanged(auth, (user) => {
+//     updateUserState(user)
+//     if (user) {
+//       if (reloadIntervalId) {
+//         clearInterval(reloadIntervalId)
+//       }
 
-      // Create reload interval
-      reloadIntervalId = setInterval(() => reloadUser(), 60 * 1000)
-    } else {
-      // Clear Reload Interval
-      clearInterval(reloadIntervalId)
-      reloadIntervalId = undefined
-    }
-  })
+//       // Create reload interval
+//       reloadIntervalId = setInterval(() => reloadUser(), 60 * 1000)
+//     } else {
+//       // Clear Reload Interval
+//       clearInterval(reloadIntervalId)
+//       reloadIntervalId = undefined
+//     }
+//   })
 
-  // Unsubscribe from auth state changes
-  onBeforeUnmount(() => {
-    clearInterval(reloadIntervalId)
-    unsubscribeAuth()
-  })
-})
+//   // Unsubscribe from auth state changes
+//   onBeforeUnmount(() => {
+//     clearInterval(reloadIntervalId)
+//     unsubscribeAuth()
+//   })
+// })
 </script>
 
 <style lang="scss" src="./scss/base.scss" />
